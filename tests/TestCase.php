8972751cde905252fb60a981a879f9762a3dd04e
Nevadskiy <nevadskiy@gmail.com>
2022-12-31 20:34:23 +0200

    Add base test case

---
+++ tests/TestCase.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Nevadskiy\Tree\Tests;
+
+use Illuminate\Foundation\Application;
+use Nevadskiy\Tree\TreeServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__.'/Support/Migrations');
+
+        $this->artisan('migrate', ['--database' => 'testbench'])->run();
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param Application $app
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            TreeServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param Application $app
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
