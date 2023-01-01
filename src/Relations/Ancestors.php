711cec3da2283a57fe7d516f698a15f6e45fafda
Nevadskiy <nevadskiy@gmail.com>
2023-01-01 19:19:48 +0200

    Fix builder namespace

--- src/Relations/Ancestors.php
+++ src/Relations/Ancestors.php
@@ -2,7 +2,7 @@
 
 namespace Nevadskiy\Tree\Relations;
 
-use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
