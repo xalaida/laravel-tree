db829568693e190d6fe27798de8aa1ca60c663b7
Nevadskiy <nevadskiy@gmail.com>
2023-01-01 19:16:51 +0200

    Use anonymous class syntax for migrations

---
+++ tests/Support/Migrations/2022_12_31_180000_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->index();
+            $table->ltree('path')->spatialIndex();
+            $table->timestamps();
+        });
+
+        Schema::table('categories', function (Blueprint $table) {
+            $table->foreignUuid('parent_id')
+                ->after('name')
+                ->nullable()
+                ->index()
+                ->constrained('categories')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
