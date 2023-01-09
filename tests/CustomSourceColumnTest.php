13b704c95ccf65b46d40b40d77749ae630d99b89
Nevadskiy <nevadskiy@gmail.com>
2023-01-10 00:28:07 +0200

    Add possibility to use custom column as path source

---
+++ tests/CustomSourceColumnTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Nevadskiy\Tree\Tests;
+
+use Illuminate\Database\Eloquent\Model;
+use Nevadskiy\Tree\AsTree;
+use Nevadskiy\Tree\Tests\Support\Factories\CategoryFactory;
+
+class CustomSourceColumnTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_builds_path_from_custom_source_column(): void
+    {
+        $parent = CategoryWithCustomSourceColumnFactory::new()->create([
+            'name' => 'books'
+        ]);
+
+        $child = CategoryWithCustomSourceColumnFactory::new()
+            ->forParent($parent)
+            ->create([
+                'name' => 'science'
+            ]);
+
+        self::assertEquals('books.science', $child->getPath()->getValue());
+        self::assertCount(1, $parent->descendants);
+        self::assertTrue($parent->descendants->first()->is($child));
+    }
+}
+
+class CategoryWithCustomSourceColumn extends Model
+{
+    use AsTree;
+
+    protected $table = 'categories';
+
+    public function getPathSourceColumn(): string
+    {
+        return 'name';
+    }
+}
+
+class CategoryWithCustomSourceColumnFactory extends CategoryFactory
+{
+    protected $model = CategoryWithCustomSourceColumn::class;
+}
