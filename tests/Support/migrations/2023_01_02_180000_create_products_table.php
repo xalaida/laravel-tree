59d5796fb852467a3acc46f5626dc3f6e95e1396
Nevadskiy <nevadskiy@gmail.com>
2023-01-13 00:37:02 +0200

    Fix CS

--- tests/Support/migrations/2023_01_02_180000_create_products_table.php
+++ tests/Support/migrations/2023_01_02_180000_create_products_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration {
+class CreateProductsTable extends Migration
+{
     /**
      * Run the migrations.
      */
