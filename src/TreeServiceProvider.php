438ed485a13bdf85639fa34374f16918d4b0ae53
nevadskiy <nevadskiy@gmail.com>
2023-04-18 21:25:41 +0300

    WIP

--- src/TreeServiceProvider.php
+++ src/TreeServiceProvider.php
@@ -41,6 +41,8 @@ class TreeServiceProvider extends ServiceProvider
      */
     private function registerLtreeType(): void
     {
+        // @todo register type using database config.
+
         Grammar::macro('typeLtree', function () {
             return 'ltree';
         });
