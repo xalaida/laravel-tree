61756eaf8b59085065747c4a834014fb64ff6426
nevadskiy <nevadskiy@gmail.com>
2023-05-05 00:42:21 +0300

    Speed up tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,20 +2,25 @@
 
 namespace Nevadskiy\Tree\Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Nevadskiy\Tree\TreeServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
+
+    /**
+     * @inheritdoc
+     */
+    protected $loadEnvironmentVariables = true;
 
     /**
      * @inheritdoc
      */
     protected function defineDatabaseMigrations(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/Database/migrations/' .config('database.default'));
+         $this->loadMigrationsWithoutRollbackFrom(__DIR__ . '/Database/migrations/' . config('database.default'));
     }
 
     /**
